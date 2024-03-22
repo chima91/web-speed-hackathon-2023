71adbc0149110c3982c649d54685c85001e1b974
chima91 <itb4769we@gmail.com>
2024-03-22 22:21:15 +0900

    feat(/): remove throttle-debounce

--- src/client/components/foundation/WidthRestriction/WidthRestriction.tsx
+++ src/client/components/foundation/WidthRestriction/WidthRestriction.tsx
@@ -14,22 +14,17 @@ export const WidthRestriction: FC<Props> = ({ children }) => {
 
   const isReady = clientWidth !== 0;
 
+  const observer = new ResizeObserver(() => {
+    const width = containerRef.current?.getBoundingClientRect().width ?? 0;
+    // 横幅を最大 1024px にする
+    setClientWidth(Math.min(width, 1024));
+  });
+
   useEffect(() => {
-    const updateClientWidth = throttle(1000, () => {
-      const width = containerRef.current?.getBoundingClientRect().width ?? 0;
-      // 横幅を最大 1024px にする
-      setClientWidth(Math.min(width, 1024));
-    });
-
-    let timer = (function tick() {
-      return setImmediate(() => {
-        updateClientWidth();
-        timer = tick();
-      });
-    })();
-
+    if (containerRef.current == null) return;
+    observer.observe(containerRef.current);
     return () => {
-      clearImmediate(timer);
+      observer.disconnect();
     };
   }, []);
 
